ba225a27d01c62207767f72327ca5a805477ed8d
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-12-31 13:27:27 +0600

    product details page change here

--- src/AllPages/CShopDetailsPage.tsx
+++ src/AllPages/CShopDetailsPage.tsx
@@ -134,7 +134,7 @@ const CShopDetailsPage = ({ params: urlParams }: { params: any }) => {
               Shop Products{" "}
             </p>
             {allProductsData?.length > 0 ? (
-              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-3">
                 {allProductsData?.map((item: any) => (
                   <ProductCard item={item} showBuyButton={true} />
                 ))}
